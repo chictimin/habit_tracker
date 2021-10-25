3cd51904f188202801bafc10870055d7a94f361b
chictimin <chictimin@gmail.com>
2021-10-25 15:36:47 +0900

    Updates

---
+++ precache-manifest.7639cb9595af4907a251bb1a4ea29c3c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7c2d4b85eb656da6f0868567e577f63f",
+    "url": "/habit_tracker/index.html"
+  },
+  {
+    "revision": "4a8b795addd196e7cdb3",
+    "url": "/habit_tracker/static/css/main.8b73e418.chunk.css"
+  },
+  {
+    "revision": "30ea2229acc1adba22ad",
+    "url": "/habit_tracker/static/js/2.e8da4bea.chunk.js"
+  },
+  {
+    "revision": "1ea337ab0f6eaf802a045a4531cad9ed",
+    "url": "/habit_tracker/static/js/2.e8da4bea.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4a8b795addd196e7cdb3",
+    "url": "/habit_tracker/static/js/main.59183536.chunk.js"
+  },
+  {
+    "revision": "dbb297bb1ca874a80b51",
+    "url": "/habit_tracker/static/js/runtime-main.bec78f78.js"
+  }
+]);
